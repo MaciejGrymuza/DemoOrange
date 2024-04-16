c4668219df2180eb2fb79cf7884fd5672b695260
Maciej_Grymuza <50072501+MaciejGrymuza@users.noreply.github.com>
2024-04-16 11:40:09 +0200

    Changes for Log In cases (#2)
    Co-authored-by: maciejgrymuza <maciej.grymuza@proton.ch>

---
+++ tests/data/texts.ts
@@ -0,0 +1,3 @@
+export const wrongCredentialsError = 'Invalid credentials';
+export const pwdLabelTxt = 'Password';
+export const userNameLabelTxt = 'Username';
--- tests/features/logIn.spec.ts
+++ tests/features/logIn.spec.ts
@@ -1,17 +1,45 @@
-import { test, expect, Page } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 import LoginPage from '../poms/LoginPage/LoginPage';
 import MainDashboard from '../poms/MainDashboard/MainDashboard';
-import { USER_NAME, USER_PWD } from '../data';
+import { USER_NAME, USER_PWD } from '../data/settingsData';
+import { pwdLabelTxt, userNameLabelTxt, wrongCredentialsError } from '../data/texts';
 
+const test = base.extend<{
+    loginPage: LoginPage,
+    mainDashboard: MainDashboard
+}>({
+    loginPage: async ({ page }, use) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.goTo();
+        await use(loginPage);
+    },
+    mainDashboard: async ({ page }, use) => {
+        await use(new MainDashboard(page))
+    }
+});
 
 test.describe('Log in test suite', () => {
-    test('Log in to the app', async ({page}) => {
-        const loginPage = new LoginPage(page);
-        const mainDashboard = new MainDashboard(page);
-
-        await loginPage.goTo();
+    test('Log in to the app', async ({loginPage, mainDashboard}) => {
+        
+        await expect(loginPage.userNameLabel).toHaveText(userNameLabelTxt);
+        await expect(loginPage.pwdLabel).toHaveText(pwdLabelTxt);
         await loginPage.logIn(USER_NAME, USER_PWD);
 
         await mainDashboard.pageLoaded();
     } )
+
+    test('Try to log in with a wrong password', async ({loginPage}) => {
+        await loginPage.goTo();
+        await loginPage.logIn(USER_NAME, 'WrongPassword');
+
+        await expect(loginPage.credentialsError).toBeVisible();
+        await expect(loginPage.credentialsError).toHaveText(wrongCredentialsError);
+    })
+
+    test('Try to log in with a wrong user name', async({loginPage})=> {
+        await loginPage.goTo();
+        await loginPage.logIn('Wrong Name', USER_PWD);
+        await expect(loginPage.credentialsError).toBeVisible();
+        await expect(loginPage.credentialsError).toHaveText(wrongCredentialsError);
+    })
 })
